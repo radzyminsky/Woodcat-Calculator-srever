81b68029439d68bccebf48f82d291d36e2d10afa
Yonatan <yonatan22195@gmail.com>
2021-11-18 13:59:57 +0200

    this version is inclouded functions that help at uses in stardant array

--- Program.cs
+++ Program.cs
@@ -11,30 +11,45 @@ namespace WoodcatCalculator
     {
         static void Main(string[] args)
         {
+
             COORDINATES cOORDINATES = new COORDINATES();
-            cOORDINATES.coordinates = new OneCoordinate[]{
-                new OneCoordinate(0, 0),
-                new OneCoordinate(5, 0),
-                new OneCoordinate(5, 5),
-                new OneCoordinate(0, 5)
-            };
-            cOORDINATES.update_type();
+            cOORDINATES.add(0, new Coordinate[]{
+                new Coordinate(0, 0),
+                new Coordinate(5, 0),
+                new Coordinate(5, 5),
+                new Coordinate(0, 5)
+            });
             Console.WriteLine(cOORDINATES);
+            Console.WriteLine("add");
+            cOORDINATES.add(3, new Coordinate[]{
+                new Coordinate(6, 5),
+                new Coordinate(6, 7),
+                new Coordinate(0, 7)
+            });
+            Console.WriteLine(cOORDINATES);
+            Console.WriteLine("remove");
+            cOORDINATES.remove(cOORDINATES.find(new Coordinate(6, 5)), cOORDINATES.find(new Coordinate(0, 7)));
+            Console.WriteLine(cOORDINATES);
+            List<Coordinate> list = new List<Coordinate>();
+            
+           
         }
     }
-    class OneCoordinate
+    class Coordinate
     {
         public double x;
         public double y;
         public types type;
-        public OneCoordinate(double x, double y)
+        public Coordinate(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
-        public OneCoordinate(double x, double y, types type)
+        public Coordinate(double x, double y, types type)
         {
-
+            this.x = x;
+            this.y = y;
+            this.type = type;
         }
 
         public override string ToString()
@@ -44,8 +59,72 @@ namespace WoodcatCalculator
     }
     class COORDINATES
     {
-        public OneCoordinate[] coordinates;
-        public void update_type()
+        private Coordinate[] coordinates;
+
+        public COORDINATES()
+        {
+
+            coordinates = new Coordinate[0];
+        }
+
+        // add "sub array" (c) to the array (coordinates) from the index paramater.
+        public void add(int from_index, Coordinate[] c)
+        {
+            Coordinate[] new_coordinates = new Coordinate[coordinates.Length + c.Length];
+            for (int i = 0; i < from_index; i++)
+            {
+                new_coordinates[i] = coordinates[i];
+            }
+            for (int i = from_index; i < c.Length + from_index; i++)
+            {
+                new_coordinates[i] = c[i - from_index];
+            }
+            for (int i = c.Length + from_index; i < new_coordinates.Length; i++)
+            {
+                new_coordinates[i] = coordinates[i - c.Length];
+            }
+            coordinates = new_coordinates;
+            update_type();
+
+
+        }
+
+        //renove from the array from index "from_index" until "till_index" inclloded "till_index".
+        public void remove(int from_index, int till_index)
+        {
+            int difference = till_index - from_index;
+            int size = coordinates.Length - (difference + 1);
+
+            Coordinate[] new_coordinates = new Coordinate[size];
+
+            for (int i = 0; i < from_index; i++)
+            {
+                new_coordinates[i] = coordinates[i];
+            }
+
+            for (int i = till_index + 1; i < coordinates.Length; i++)
+            {
+                new_coordinates[i - (difference + 1)] = coordinates[i];
+            }
+
+            coordinates = new_coordinates;
+            update_type();
+
+        }
+
+        //return index of given parameter or -1 if isn't found.
+        public int find(Coordinate oc)
+        {
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                if (coordinates[i].x == oc.x && coordinates[i].y == oc.y)
+                    return i;
+            }
+            return -1;
+        }
+
+        //insert value to "type" enum field.
+        private void update_type()
         {
 
             for (int i = 0; i < coordinates.Length; i++)
@@ -69,12 +148,35 @@ namespace WoodcatCalculator
 
         }
 
+        //The function receives an index and checks whether the point is external.
+        public bool check_if_is_external(int index)
+        {
+            int previous = index - 1;
+            if (index == 0)
+                previous = coordinates.Length - 1;
+            if (coordinates[index].type == types.left && coordinates[previous].type == types.down)
+                 return false;
+            if (coordinates[index].type == types.right && coordinates[previous].type == types.top)
+                return false;
+            if (coordinates[index].type == types.top && coordinates[previous].type == types.left)
+                return false;
+            if (coordinates[index].type == types.down && coordinates[previous].type == types.right)
+                return false;
+            return true;
+        }
+
+        
+
         public override string ToString()
         {
-            string str="";
-            foreach (OneCoordinate oc in coordinates)
+            string str = "";
+            foreach (Coordinate oc in coordinates)
             {
-                str += oc+" ~~ ";
+                if (check_if_is_external(find(oc)))
+                    str += "(externul)";
+                else
+                    str += "(internal)";
+                str += oc + " ~~ ";
             }
             return str;
         }
@@ -88,6 +190,7 @@ namespace WoodcatCalculator
         public double Width;
         public piese(double length, double width)
         {
+            //the "length" allwais needs biger from width
             if (width > length)
             {
                 Length = width;
